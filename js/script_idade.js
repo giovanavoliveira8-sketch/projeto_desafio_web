6a72ea9af10f10f01bb6017604b57b6f83c6e623
Giovana <Giovana.v.oliveira8@aluno.senai.br>
2026-06-12 16:17:19 -0300

    Modificado o arquivo js/script_idade.js

--- js/script_idade.js
+++ js/script_idade.js
@@ -1,25 +1,27 @@
 //PEGANDO ELEMENTOS DO DOM
-const formDados = document.querySelector('#formulário pessoa')
+const formDados = document.querySelector('#formulario-pessoa')
 const divResultado = document.querySelector('#div-dados')
 
 //CAPTURANDO O EVENTO SUBMIT [X] FORMULÁRIO
 formDados.addEventListener ('submit', (evt)=>{
     evt.preventDefault()
+
     
-    const objFormDados =new FormData (formDados)
-
+    const objFormDados =new FormData(formDados)
+    
     let nome = objFormDados.get('nome')
-    let idade = objFormDados.get ('idade')
-
+    let idade = objFormDados.get('idade')
+    
     let situacaoIdade = ''
-
+    
     if (idade >= 18){
-        situacaoIdade - `${nome}, você é maior de idade`
+        situacaoIdade = `${nome}, você é maior de idade`
     }else{
-        situacaoIdade - `${nome}, você é menor de idade`
+        console.log('aqio')
+        situacaoIdade = `${nome}, você é menor de idade`
     }
 
     divResultado.innerHTML = situacaoIdade
 
-    
+
 })
