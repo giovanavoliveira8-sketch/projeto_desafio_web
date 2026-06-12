733007dcbc0a1bfccf6f7f3d7b3c65c881775a93
Giovana <Giovana.v.oliveira8@aluno.senai.br>
2026-06-12 14:58:55 -0300

    Capturado o evento submit

--- js/script_idade.js
+++ js/script_idade.js
@@ -2,3 +2,9 @@
 const formDados = document.querySelector('#formulário pessoa')
 const divResultado = document.querySelector('#div-dados')
 
+//CAPTURANDO O EVENTO SUBMIT [X] FORMULÁRIO
+formDados.addEventListener ('submit', (evt)=>{
+    evt.preventDefault()
+    
+    
+})
